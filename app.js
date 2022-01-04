eab8fc7fddb7a4ca6db3d5a50fce2f10a956a696
VINAY KAITHWAS <vinay.kaithwas@expresstechsoftwares.com>
2022-01-04 19:08:06 +0530

    improvement-crud-api-withoutdb

--- app.js
+++ app.js
@@ -1,20 +1,84 @@
 const express = require("express");
-const fs = require("fs");
-const req = require("express/lib/request");
-const res = require("express/lib/response");
+const { json } = require("express/lib/response");
 const app = express();
-const port = 3001;
-app.listen(port, () => {
-    console.log(`App running on port ${port}...`);
-});
 
+app.use(express.json());
+
+const fs = require("fs");
 const tours = JSON.parse(fs.readFileSync(`${__dirname}/dev-data/tours.json`));
 
-app.get('/api/v1/tours', (req, res) => {
-    res.status(200).json({
-        status : "Success",
-        data: {
-            tours
-        }
-    });
+const port = 3000;
+app.listen(port, () => {
+    console.log(`App running on port ${port}`);
 });
+
+//Custom middleware
+app.use((req, res, next) => {
+    console.log('Hello i am from middleware');
+    next();
+});
+//Callbacks
+const getAllTours =
+    (req, res) => {
+        res.status(200).json({
+            status: "Success",
+            data: {
+                tours
+            }
+        });
+    }
+
+
+const createTour =
+    (req, res) => {
+        const bodyData = req.body;
+        res.status(200).json({
+            status: "Success",
+            data: bodyData
+        });
+    }
+
+const deleteTour =
+    (req, res) => {
+        const id = req.params.id;
+
+        res.status(200).json({
+            message: "Id fetched from url",
+            status: "Success",
+            id: id
+        });
+    }
+
+const updateTour =
+    (req, res) => {
+        const id = req.params.id;
+
+        res.status(200).json({
+            message: "Id fetched from url",
+            status: "Success",
+            id: id
+        });
+    }
+
+const getTour =
+    (req, res) => {
+        const id = req.params.id;
+
+        res.status(200).json({
+            message: "Id fetched from url",
+            status: "Success",
+            id: id
+        });
+    }
+
+
+//Routes
+app.route('/api/v1/tours')
+    .get(getAllTours)
+    .post(createTour);
+
+app
+    .route('/api/v1/tours/:id')
+    .get(getTour)
+    .patch(updateTour)
+    .delete(deleteTour);
